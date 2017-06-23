d7f6a6130d21867c29059b5e33d10389aae338fd
Eric <eclam@sfu.ca>
2017-06-23 13:32:56 -0700

    connect eric

---
+++ test/untitled.rb
@@ -0,0 +1,9 @@
+print "fuck you world\n"
+
+vagina_num = gets.to_i
+printf "Insert Dick Value: "
+asscrack_num = gets.to_i
+
+puts "CUMTASTIC"
+
+puts vagina_num.to_s + " + " + asscrack_num.to_s + "= EJACULATION + ORGASMS"
