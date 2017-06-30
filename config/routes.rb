c91ce663aaa796a42dbd13f32c0e9c3b2b4a3020
Bryce <brycegolamco@gmail.com>
2017-06-30 10:10:19 +0000

    Profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'welcome', to: 'welcome#index'
+  get 'welcome/profile', to: 'welcome#profile'
 
   root 'welcome#index'
 end
