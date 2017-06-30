a248c0e76362d7fd46c02ccdcaa2068bb6d0f239
DavidGuo <dxguo@sfu.ca>
2017-06-30 00:23:55 -0700

    Used devise to change schema (made users authenticate-able). Generated the devise authentication views. Authentication still buggy because Username field not working properly and is required in the User model (validates :username, presence:true)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	validates :password, presence:true
 	validates :username, presence:true 
 	validates :email, presence:true
