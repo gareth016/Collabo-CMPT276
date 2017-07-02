577f1f0699fc91fc0159258d45781198805b58a7
Eric <eclam@sfu.ca>
2017-07-02 14:39:41 -0700

    added isAdmin to db user table

---
+++ db/migrate/20170702213158_addis_admin_to_users.rb
@@ -0,0 +1,8 @@
+class AddisAdminToUsers < ActiveRecord::Migration[5.0]
+  def change
+  	 change_table :users do |t|
+      ## Trackable
+      t.boolean  :is_Admin, default: false, null: false
+    end
+  end
+end
