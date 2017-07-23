c196dbd04e894bda8fb647d1daa05219b392c151
Eric <eclam@sfu.ca>
2017-07-22 18:21:40 -0700

    trying to pull properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     @user.destroy
+    
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
