2f90d966d75eb8a1438e98a49256b16fb38b35f3
Bryce <brycegolamco@gmail.com>
2017-07-24 21:36:16 +0000

    Resolve merges

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'rails', '~> 5.0.2'
 # Use mysql as the database for Active Record
 gem 'sqlite3'
-#gem 'mysql2', '>= 0.3.18', '< 0.5'
+gem 'mysql2', '>= 0.3.18', '< 0.5'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,7 +73,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:id, :password, :email)
+      params.require(:user).permit(:id, :password, :email, :avatar)
     end
 
   protected
